be888eba97c0422114aee2154a30d03ff49e2718
cmvcordova <cmvcordova@pm.me>
2025-02-05 18:21:05 -0500

    Added slides download script

---
+++ Slides/download_slides.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script downloads the workshop slides from Google Drive
+# File URL: https://drive.google.com/file/d/1OlVJvRKx11RvBhNXLH61ZtDp2PogzKgY/view?usp=drive_link
+# File ID: 1OlVJvRKx11RvBhNXLH61ZtDp2PogzKgY
+
+gdown 1OlVJvRKx11RvBhNXLH61ZtDp2PogzKgY -O intro-to-unix.pdf 
+
+echo "Slides download complete!"
+
